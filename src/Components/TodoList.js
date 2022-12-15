bcdcc441b67f17097d9a1aeed7fd4dfdbff977a6
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-15 13:05:09 +0100

    corrected ternary condition

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -25,7 +25,7 @@ function TodoList() {
   };
 
   const removeTodo = (id) => {
-    const removeArr = [...todos].filter((todo) => todo.id != id);
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
 
     setTodos(removeArr);
   };
