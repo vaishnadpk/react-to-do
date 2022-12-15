47c329a142c5498af7a96a925a49505f8aa31311
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-15 12:27:13 +0100

    updated updatetodo

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -14,6 +14,16 @@ function TodoList() {
     console.log(todo, ...todos);
   };
 
+  const updateTodo = (todoId, newValue) => {
+    if (!todoId.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
   const removeTodo = (id) => {
     const removeArr = [...todos].filter((todo) => todo.id != id);
 
@@ -33,7 +43,12 @@ function TodoList() {
     <div>
       <h1>What's your plan for Today ?</h1>
       <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
     </div>
   );
 }
