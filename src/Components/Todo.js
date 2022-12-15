6cd76635f0c79e42d9bd12fb115e4be0ee572c59
vaishnadpk <107250778+vaishnadpk@users.noreply.github.com>
2022-12-15 13:03:57 +0100

    updated

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -2,12 +2,24 @@ import React from "react";
 import { useState } from "react";
 import { RiCloseCircleLine } from "react-icons/ri";
 import { TiEdit } from "react-icons/ti";
+import TodoForm from "./TodoForm";
 
-function Todo(todos, completeTodo, removeTodo) {
+function Todo(todos, completeTodo, removeTodo, updateTodo) {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
   });
+  const submitUpdate = (value) => {
+    updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: "",
+    });
+  };
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
   return todos.map((todo, index) => (
     <div
       className={todo.isComplete ? "todo-row-complete" : "todo-row"}
